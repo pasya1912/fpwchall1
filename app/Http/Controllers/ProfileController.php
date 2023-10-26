49d087e3b26709ad517a56b976e40a8c89fb5f44
Rafli Pasya <androgaming1912@gmail.com>
2023-10-26 15:42:35 +0700

    Menambah hobby

--- app/Http/Controllers/ProfileController.php
+++ app/Http/Controllers/ProfileController.php
@@ -9,7 +9,7 @@ class ProfileController extends Controller
     public function index(){
         $nama = "Rafli Pasya";
         $bio = "Saya mahasiswa UNSIKA Informatika";
-        $hobby = [];
+        $hobby = ["Gaming","Gym","Nonton"];
         return view('biodata',['nama'=>$nama,'deskripsi'=>$bio,'hobby'=>$hobby]);
     }
 }
